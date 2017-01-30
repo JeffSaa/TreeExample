cabd06ad59c47ec2dfd8d4010db1952eac8f8176
Jeffrey Saavedra <jeffreysaa@outlook.com>
2017-01-30 17:24:21 -0500

    Node class created

--- src/treeexample/TreeExample.java
+++ src/treeexample/TreeExample.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package treeexample;
 
 /**
@@ -10,12 +5,9 @@ package treeexample;
  * @author jeff
  */
 public class TreeExample {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
