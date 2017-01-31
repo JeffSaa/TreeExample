c7b63feb88d0c3a0e93d54b0a3b7dd8b42e7e7f6
Jeffrey Saavedra <jeffreysaa@outlook.com>
2017-01-31 12:54:30 -0500

    ExistValue function ready

--- src/View/Index.java
+++ src/View/Index.java
@@ -11,19 +11,25 @@ import javax.swing.tree.DefaultMutableTreeNode;
 public class Index extends javax.swing.JFrame {
     
     Node root;
+    Boolean sw;
     
     public Index() {
         initComponents();
         root = new Node("1");
+        Node n1 = new Node("2");
+        root.setRigth(n1);
     }
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        exist_text = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        exist_text.setText("jTextField1");
 
         jButton1.setText("jButton1");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -36,24 +42,30 @@ public class Index extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(274, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(exist_text, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addGap(29, 29, 29))
+                .addContainerGap(446, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(265, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addContainerGap())
+                .addContainerGap(430, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(exist_text, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addGap(31, 31, 31))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       
+        sw = false;
+        existValue(root, "2");
+        System.out.println();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     public static void main(String args[]) {
@@ -86,10 +98,6 @@ public class Index extends javax.swing.JFrame {
 
     @Override
     public void paint(Graphics g) {
-        Node l = new Node("2"), l2 = new Node("3"), l3 = new Node("6");
-        root.setRigth(l2);
-        l2.setLeft(l);
-        l2.setRigth(l3);
         drawTree(g,150,50,root);
     }
     
@@ -105,8 +113,24 @@ public class Index extends javax.swing.JFrame {
         }
     }
     
+    private void existValue(Node n, String q){
+        if (q.equals(n.getName())) {
+            sw = true;
+            System.out.println("Si");
+        }
+        else{
+            if (n.getLeft() != null) {
+                existValue(n.getLeft(), q);
+            }
+            if (n.getRigth()!= null) {
+                existValue(n.getRigth(), q);
+            }
+        }
+    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField exist_text;
     private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
